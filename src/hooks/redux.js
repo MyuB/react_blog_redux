71e24c1590e29ed3c1ecde03a76762bffbd95b4d
MyuB <mjycoco0@naver.com>
2024-01-01 15:22:04 +0900

    feat: redux를 활용한 store 생성

---
+++ src/hooks/redux.js
@@ -0,0 +1,8 @@
+import { useDispatch, useSelector } from "react-redux";
+//import { AppDispatch, RootState } from "../store";
+
+//export const useAppDispatch = () => useDispatch<AppDispatch>();
+//export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const useAppDispatch = () => useDispatch();
+export const useAppSelector = useSelector;
